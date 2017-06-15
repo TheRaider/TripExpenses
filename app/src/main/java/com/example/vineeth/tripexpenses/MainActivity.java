7520c96f63a0bc92fa7fa5884cde9a312c21ae22
SaiKrishna <chsaikrishna105@gmail.com>
2017-06-15 13:42:59 +0530

    button added

--- app/src/main/java/com/example/vineeth/tripexpenses/MainActivity.java
+++ app/src/main/java/com/example/vineeth/tripexpenses/MainActivity.java
@@ -2,21 +2,40 @@ package com.example.vineeth.tripexpenses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     String TripName;
+
+    Button submitButton;
+    TextView yahoo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(getSupportActionBar()!=null){
+        if(getSupportActionBar()!=null) {
             getSupportActionBar().setTitle("Home");
 
 
 
         }
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        yahoo = (TextView) findViewById(R.id.yahoo);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                yahoo.setText("Button is pressed");
+            }
+        });
 
     }
 }
